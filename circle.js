4219b0989847085dad0dfdf585311e53a0a371b0
Erik Weitnauer <eweitnauer@gmail.com>
2013-02-21 16:08:46 +0100

    Circle.fromSVGPath does not need an nsResolver as parameter anymore. Instead it takes an optional parameter that can be used to allow rx != ry cases (and just take (rx+ry)/2 as radius)

--- circle.js
+++ circle.js
@@ -25,15 +25,21 @@ Circle.prototype.move_to_origin = function() {
 // If the passed path SVG element was originally a circle and written as a path
 // by inkscape, it will construct a new Circle object from it and return it. If
 // the path does not resemble a circle, it will return null.
-Circle.fromSVGPath = function(path_node, nsResolver) {
-  var ns = nsResolver.lookupNamespaceURI('sodipodi');
+// If exclude_ellipse is passed as true, the program will reject cases in which
+// rx differs from ry (default: true).
+Circle.fromSVGPath = function(path_node, exclude_ellipse) {
+  if (typeof(exclude_ellipse) == 'undefined') exclude_ellipse = true;
+  var ns = path_node.lookupNamespaceURI('sodipodi');
   var get_attr = function(attr) { return path_node.getAttributeNS(ns, attr) };
   var cx, cy, rx, ry;
   if (get_attr('type') == 'arc' && (cx = get_attr('cx')) && (cy = get_attr('cy')) &&
       (rx = get_attr('rx')) && (ry = get_attr('ry')))
   {
     rx = Number(rx); ry = Number(ry); cx = Number(cx); cy = Number(cy);
-    if (Math.abs(rx-ry) > Point.EPS) return null;
+    if (Math.abs(rx-ry) > Point.EPS) {
+      console.log("Warning: This is an ellipse, no circle! rx", rx, "ry", ry);
+      if (exclude_ellipse) return null;
+    }
     return new Circle(cx, cy, (rx+ry)/2);
   }
   return null;
