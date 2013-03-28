b4df40d4108096a29102d30d78ac1debb217f50c
Erik Weitnauer <eweitnauer@gmail.com>
2013-03-28 09:49:17 +0100

    adapted near and far membership function. correct debug drawing in getMembership function.

--- spatial_relation_analyzer.js
+++ spatial_relation_analyzer.js
@@ -61,12 +61,14 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
   }
 
   function near_membership(val) {
-    return 1-1/(1+Math.exp(25/res*(res/8-val)));
+    return 1-1/(1+Math.exp(30*(0.2-val/res/scale)));
+    //return 1-1/(1+Math.exp(25/res*(res/12-val)));
   }
 
   function far_membership(val) {
-    if (val == 0) return 0;
-    return 1/(1+Math.exp(25/res*(res/4-val)));
+    return 1/(1+Math.exp(20*(0.35-val/res/scale)));
+    //if (val == 0) return 0;
+    //return 1/(1+Math.exp(25/res*(res/4-val)));
   }
 
   core.beta_fs = {'right': beta_right, 'left': beta_left,
@@ -82,14 +84,18 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
   /// Returns the [min, avg, max] membership values for the given spatial relationship
   /// type. Calculates body_matrix of A and spatial landscape of R if neccessary.
   core.getMembership = function(A_shape, R_shape, debug_draw) {
-    var val = calcObjectMembership(getSpatialMembershipMap(R_shape)
-                               ,getBodyMatrix(A_shape)
-                               ,scale*(A_shape.x - R_shape.x)
-                               ,scale*(A_shape.y - R_shape.y));
-    if (debug_draw) core.debug_draw_A_R(getBodyMatrix(A_shape)
-                                       ,getSpatialMembershipMap(R_shape)
-                                       ,scale*(A_shape.x - R_shape.x)
-                                       ,scale*(A_shape.y - R_shape.y));
+    var R_map = core.getSpatialMembershipMap(R_shape)
+       ,A = core.getBodyMatrix(A_shape)
+       ,dx = scale*(A_shape.x - R_shape.x)
+       ,dy = scale*(A_shape.y - R_shape.y);
+
+    var val = calcObjectMembership(A, R_map, dx, dy);
+
+    if (debug_draw) {
+      var can = core.debug_get_canvas('debug');
+      core.debug_draw_A_R(A, R_map, can, dx, dy);
+    }
+
     return val;
   }
 
@@ -100,12 +106,12 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
 
   /// Will store one map per shape and type and reuse it if resolution did not change and
   /// rotation did not change by more than max_drot.
-  var getSpatialMembershipMap = function(shape) {
+  core.getSpatialMembershipMap = function(shape) {
     if (!shape._spatial_maps) shape._spatial_maps = {};
     var map = shape._spatial_maps[type]
     if (!map || map.res != res || !is_in(map.rot, shape.rot-max_drot, shape.rot+max_drot)) {
       map = shape._spatial_maps[type] = {res: res, rot: shape.rot
-                                        ,data: calcSpatialMembershipMapFaster(getBodyMatrix(shape)
+                                        ,data: calcSpatialMembershipMapFaster(core.getBodyMatrix(shape)
                                         ,core.f_beta, core.f_member)};
     }
     return map.data;
@@ -113,7 +119,7 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
 
   /// Will store one body matrix per shape and reuse it if resolution did not change
   /// and rotation did not change by more that ~ 2 deg.
-  var getBodyMatrix = function(shape) {
+  core.getBodyMatrix = function(shape) {
     var bm = shape._body_matrix;
     if (!bm || bm.res != res || !is_in(bm.rot, shape.rot-max_drot, shape.rot+max_drot)) {
       bm = shape._body_matrix = {res: res, rot: shape.rot
@@ -145,7 +151,7 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
 
     // make sure we have at least a single pixel
     var B = canvasToMatrix(can);
-    B[Math.floor(h/2)][Math.floor(w/2)] = 1;
+    if (B.bounding_box.x0>B.bounding_box.x1) B[Math.floor(h/2)][Math.floor(w/2)] = 1;
 
     return B;
   }
@@ -216,9 +222,9 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
 
     // translate O reference points to memberships
     for (var i=0;i<M;i++) for (var j=0;j<N;j++) {
-      if (R[i][j]==1) O[i][j] = -1;
+      if (R[i][j]==1) O[i][j] = NaN;
       else if (O[i][j]) O[i][j] = f_mem(O[i][j][2])
-      else O[i][j] = -1;
+      else O[i][j] = NaN;
     }
     //console.log('spatial_membership_fast: ' + (Date.now()-t_start) + ' ms.');
     return O;
@@ -226,6 +232,7 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
 
   /// Calculates and returns the spatial membership map for the passed body matrix.
   /// Faster version which uses 4-neighborhood, 2 1/4 passes and some optimizations.
+  /// The returned matrix is NaN at all places the object is.
   var calcSpatialMembershipMapFaster = core.calcSpatialMembershipMapFaster =
   function(R, f_rel, f_mem) {
     //var t_start = Date.now();
@@ -237,43 +244,43 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
       // is this a point in the reference object?
       O[i][j] = (R[i][j] == 1) ? [i,j,0] : [-1,-1,Infinity];
     }
-    var pass = function(i,j,forward) {
+    var pass = function(i, j, right, down) {
       var current = O[i][j];
       var changed = false;
       var check = function(ref) {
         if (ref[0] == -1) return;
         var rel = f_rel(j-ref[1], i-ref[0]);
-        if (rel <= current[2]) {
+        if (rel < current[2]) {
           current[0] = ref[0]; current[1] = ref[1]; current[2] = rel;
           changed = true;
         }
       }
-      // use 4-neighborhood in the right direction
-      if (forward) {
-        if (i>0) check(O[i-1][j]);
-        if (j>0) check(O[i][j-1]);
-        if (changed) O[i][j] = current; // take the best reference point
-      } else {
-        if (i<M1) check(O[i+1][j]);
-        if (j<N1) check(O[i][j+1]);
-        if (changed) O[i][j] = current; // take the best reference point
-      }
+      if (right && j>0) check(O[i][j-1]); // left
+      if (!right && j<N1) check(O[i][j+1]); // right
+      if (down && i>0) check(O[i-1][j]); // above
+      if (!down && i<M1) check(O[i+1][j]); // below
+      if (right && down && j>0 && i>0) check(O[i-1][j-1]); // left above
+      if (right && !down && j>0 && i<M1) check(O[i+1][j-1]); // left below
+      if (!right && down && j<N1 && i>0) check(O[i-1][j+1]); // right above
+      if (!right && !down && j<N1 && i<M1) check(O[i+1][j+1]); // right below
+
+      if (changed) O[i][j] = current;
     }
 
-    // first pass: forward iteration over O
-    var y0 = R.bounding_box ? Math.max(0, R.bounding_box.y0-1) : 0;
-    var x0 = R.bounding_box ? Math.max(0, R.bounding_box.x0-1) : 0;
-    for (var i=y0;i<M;i++) for (var j=x0;j<N;j++) pass(i,j,true);
-    // second pass: backward iteration over O
-    for (var i=M1;i>=0;i--) for (var j=N1;j>=0;j--) pass(i,j, false);
-    // third pass: forward iteration
-    for (var i=0;i<M;i++) for (var j=0;j<N;j++) pass(i,j,true);
+    var y0 = R.bounding_box ? Math.max(0, R.bounding_box.y0) : 0;
+    var x0 = R.bounding_box ? Math.max(0, R.bounding_box.x0) : 0;
+    var y1 = R.bounding_box ? Math.max(0, R.bounding_box.y1) : M1;
+    var x1 = R.bounding_box ? Math.max(0, R.bounding_box.x1) : N1;
+    for (var i=y0;i<M;i++) for (var j=x0;j<N;j++) pass(i,j,true,true);
+    for (var i=y0;i<M;i++) for (var j=x1;j>=0;j--) pass(i,j,false,true);
+    for (var i=y1;i>=0;i--) for (var j=x0;j<N;j++) pass(i,j,true,false);
+    for (var i=y1;i>=0;i--) for (var j=x1;j>=0;j--) pass(i,j,false,false);
 
     // translate O reference points to memberships
     for (var i=0;i<M;i++) for (var j=0;j<N;j++) {
-      if (R[i][j]==1) O[i][j] = -1;
+      if (R[i][j]==1) O[i][j] = NaN;
       else if (O[i][j][2] != Infinity) O[i][j] = f_mem(O[i][j][2])
-      else O[i][j] = -1;
+      else O[i][j] = NaN;
     }
     //console.log('spatial_membership_faster: ' + (Date.now()-t_start) + ' ms.');
     return O;
@@ -281,10 +288,10 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
 
   /// Calculates the membership of the object in the matrix A when placed at
   /// 'x', 'y' in the membership landscape defined by the matrix R.
-  /// Points in 'set' that are -1 are ignored. E.g., all points of the reference
-  /// object can be marked as -1.
+  /// Points in 'set' that are NaN are ignored. E.g., all points of the reference
+  /// object can be marked as NaN.
   /// Returns three measures [min=necessity, average, max=possibility].
-  var calcObjectMembership = function(R, A, x, y) {
+  var calcObjectMembership = core.calcObjectMembership = function(A, R, x, y) {
     //var t_start = Date.now();
     var max = null; // possibility
     var avg = 0;    // average
@@ -302,7 +309,7 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
       if (ri>=R.M || ri<0) continue; // skip points outside 'R'
       if (rj>=R.N || rj<0) continue; // skip points outside 'R'
       var val = R[ri][rj];
-      if (val == -1) continue; // skip points that overlap with reference Object
+      if (isNaN(val)) continue; // skip points that overlap with reference Object
       min = (min === null) ? val : Math.min(min, val);
       avg += val;
       num++;
@@ -314,60 +321,77 @@ var SpatialRelationAnalyzer = function(resolution, scale, type, f_beta, f_member
     return [min, avg, max];
   }
 
-  var createCanvas = function(id, w, h) {
-    return d3.select("body")
-             .append('canvas')
-             .attr('id', id)
-             .attr('width', w)
-             .attr('height', h)
-             .style('width', '300px')
-             .style('height', '300px')[0][0];
+  /// Creates a new canvas with the given id. If a canvas with that id existed before, its
+  /// reused but cleared.
+  core.debug_get_canvas = function(id, caption, w, h) {
+    w = w || 200; h = h || 200;
+    var can = d3.select("canvas#"+id);
+    if (can.empty()) {
+      var div = d3.select("body")
+                  .append('div')
+                  .classed('debug-can-div', true)
+                  .style('text-align', 'center');
+      can = div.append('canvas')
+               .attr('id', id);
+      if (caption) {
+        div.append('br');
+        div.append('span')
+           .text(caption);
+      }
+    }
+    can.attr('width', w)
+       .attr('height', h);
+    can[0][0].getContext('2d').clearRect(0,0,w,h);
+
+    return can[0][0];
   }
-  /// Draws the matrix `A` onto a canvas with side length `size` using w and h
-  /// as the number of cells in x and y direction. If w and h are not set, A.N and
-  /// A.M are used.
-  core.debug_draw_matrix = function(A, size) {
-    size = size || 300;
-    w = A.N; h = A.M;
-    var cell = size / Math.max(w, h);
-    var can = d3.select('canvas#debug')[0][0] || createCanvas('debug', size, size);
+
+  /// Draws the matrix `A` onto the canvas. If color is not given, white is used.
+  core.debug_draw_matrix = function(A, can, color, dx, dy) {
+    color = color || 'white';
+    dx = dx || 0; dy = dy || 0;
+    dx = Math.round(dx); dy = Math.round(dy);
+    var w = A.N, h = A.M;
+    var cell = Math.min(can.width/w, can.height/h);
     var ctx = can.getContext("2d");
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0,0,size,size);
     for (var i=0; i<A.M; i++) for (var j=0; j<A.N; j++) {
-       var val = Math.floor(A[i][j]*255);
-       ctx.fillStyle = A[i][j] == -1 ? 'red' : 'rgb(' + val + ',' + val + ',' + val + ')';
-       ctx.fillRect(j*cell, i*cell, cell+1, cell+1);
+      var col;
+      if (isNaN(A[i][j])) {
+        col = 'red';
+        ctx.globalAlpha = 1;
+      } else {
+        col = color;
+        ctx.globalAlpha = A[i][j] > 0 ? A[i][j] : 0;
+      }
+      ctx.fillStyle = col;
+      var x0 = Math.round((j+dx)*cell), y0 = Math.round((i+dy)*cell)
+         ,x1 = Math.round((j+1+dx)*cell), y1 = Math.round((i+1+dy)*cell);
+      ctx.fillRect(x0, y0, x1-x0, y1-y0);
     }
   }
 
   /// Draws the spatial membership landscape in R and then the object in matrix
-  /// A on top of it, using x, y and rot as transformation.
-  core.debug_draw_A_R = function(A, R, dx, dy, size) {
-    size = size || 300;
-    var w, h, cell;
-    if (R) {
-      w = R.N; h = R.M;
-      cell = size / Math.max(w, h);
-      core.debug_draw_matrix(R, size);
-    }
-    if (A) {
-      w = A.N; h = A.M;
-      cell = size / Math.max(w, h);
-      var can = d3.select('canvas#debug')[0][0] || createCanvas('debug', size, size);
-      var ctx = can.getContext("2d");
-      ctx.fillStyle = 'green';
-      dx = Math.round(dx); dy = Math.round(dy);
-      var w2 = A.N/2, h2 = A.M/2;
-      for (var i=0; i<A.M; i++) for (var j=0; j<A.N; j++) {
-        if (!A[i][j]) continue;
-        var x = Math.round(j+dx), y = Math.round(i+dy);
-        ctx.fillRect(x*cell, y*cell, cell, cell);
-      };
-      ctx.fillStyle = 'lightgreen';
-      ctx.fillRect((w2-0.5+dx)*cell, (h2-0.5+dy)*cell, cell, cell);
-    }
+  /// A on top of it, using dx, dy as transformation.
+  core.debug_draw_A_R = function(A, R, can, dx, dy) {
+    var ctx = can.getContext('2d');
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,can.width,can.height);
+
+    if (R) core.debug_draw_matrix(R, can);
+    if (A) core.debug_draw_matrix(A, can, 'rgba(0,0,255,0.5)', dx, dy);
   }
 
   return core;
 };
+
+SpatialRelationAnalyzer.conjunction = function(A, B) {
+  return A.combine(B, function(a,b) { return Math.min(a,b) });
+}
+
+SpatialRelationAnalyzer.disjunction = function(A, B) {
+  return A.combine(B, function(a,b) { return Math.max(a,b) });
+}
+
+SpatialRelationAnalyzer.subtract = function(A, B) {
+  return A.combine(B, function(a,b) { return Math.max(0, a-b) });
+}
